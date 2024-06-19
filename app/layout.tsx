3474f52bf503682a67031e8d648c1c2929a0c01c
Lesly <leslysoftwarespecialist@gmail.com>
2024-06-19 14:10:15 -0700

    Intro done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
+//import dependencies
 import "./globals.css";
 import { Inter } from "next/font/google";
+
+//import components
+import Header from "@/Components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-200 text-gray-950 relative`}>
+      <body className={`${inter.className} bg-gray-200 text-gray-950 relative pt-28 sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+//import components
+import Intro from '@/Components/intro'
 
 export default function HomePage() {
   return (
-    <main className="">
-     
+    <main className="flex flex-col items-center px-4">
+      <Intro />
     </main>
   )
 }
