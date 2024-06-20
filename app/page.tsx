e6c945ed45f6e02bc1ef2421a131cd630ac2e2b0
Lesly <leslysoftwarespecialist@gmail.com>
2024-06-20 03:12:24 -0700

    About Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 //import components
 import Intro from '@/Components/intro'
+import SectionDivider from '@/Components/section-divider'
+import About from "@/Components/about"
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
+      <About />
     </main>
   )
 }
