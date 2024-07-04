466ce7e1aed1eacac405213d90a0961f05689bd0
Lesly <leslysoftwarespecialist@gmail.com>
2024-07-04 01:48:13 -0700

    project page

--- Components/section-heading.tsx
+++ Components/section-heading.tsx
@@ -9,7 +9,7 @@ const SectionHeading = ({
     children }: SectionHeadingProps
 ) => {
   return (
-    <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 //import components
-import Intro from '@/Components/intro'
-import SectionDivider from '@/Components/section-divider'
-import About from "@/Components/about"
+import Intro from "@/Components/intro";
+import SectionDivider from "@/Components/section-divider";
+import About from "@/Components/about";
+import Projects from "@/Components/projects";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center px-4">
+    <main className="flex flex-col items-center px-4 h-[1000vh]">
       <Intro />
       <SectionDivider />
       <About />
+      <SectionDivider />
+      <Projects />
     </main>
-  )
+  );
 }
