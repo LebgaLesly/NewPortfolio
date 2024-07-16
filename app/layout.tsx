e97e6a2bbe0c331b2fc47d0a6414b66ae3e5bbe2
Lesly <leslysoftwarespecialist@gmail.com>
2024-07-16 13:16:17 -0700

    changes made

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,11 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 //import components
-import Header from "@/components/Header";
 import ActiveSectionContextProvider from "@/context/active-section";
 import { Toaster } from "react-hot-toast";
-import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
+import Header from "@/components/Header";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 //import components
-import Intro from "@/components/intro";
-import SectionDivider from "@/components/section-divider";
-import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
+import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
+import About from "@/components/about";
 
 export default function HomePage() {
   return (
